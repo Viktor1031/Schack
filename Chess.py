e4bce7e3e549c88c2058d4ce1d183cc753d37a59
Viktor Lindfors <viktorlindfors0@gmail.com>
2024-10-19 13:29:42 +0200

    snyggare kod

--- Chess.py
+++ Chess.py
@@ -11,6 +11,7 @@ sträng_till_kolumn_katalog={
     "f" : 5,
     "g" : 6,
     "h" : 7,
+    "i" : 8,
 }
 kolumn_till_sträng_katalog={
     0 : "a",
@@ -21,9 +22,11 @@ kolumn_till_sträng_katalog={
     5 : "f",
     6 : "g",
     7 : "h",
+    8 : "i",
 }
 
 def sträng_position_till_vektor2(sträng):
+    
     kolumn_sträng = sträng[0]  # Bokstaven, t.ex. "e"
     rad_sträng = sträng[1]     # Siffran, t.ex. "2"
     
@@ -32,25 +35,62 @@ def sträng_position_till_vektor2(sträng):
     
     return (kolumn,rad)
 
-
-
-#flyttfunktion förflyttning x, y per steg sen stopvärde så häst (+-1,+-2,1) (+-2,+-1,1)
-#Bonde start 2 steg, krav moves=0. Flytt funktion (0,2,1)
-#Bonde fram 1 steg, flytt funktion (0,1,1)
-#Bonde sida krav(pjäs.x,pjäs.y)  
-
-#Y mirror x mirror
-
-def är_vektor2_i_matris(vektor2,matris):
+def är_vektor2_i_matris(vektor2, matris):
     if (vektor2[0] in range(len(matris))) and (vektor2[1] in range(len(matris[0]))):
         return True
     return False
 
+def är_vektor2_en_icke_valid_kollision_med_annan_pjäs(vektor2, schackbräde_matris, beteende, pjäs):
+    
+    x=vektor2[0]
+    y=vektor2[1]
+    
+    annan_pjäs=schackbräde_matris[x][y].pjäs
+    
+    if annan_pjäs!=None:
+        if (beteende.kan_döda==True) and (annan_pjäs.färg!=pjäs.färg):
+            return True
+        else:
+            return False
+    return True
+            
+class Villkor:
+    def __init__(self, villkors_funktion, **villkor_keyword_argument):
+        self.villkors_funktion = villkors_funktion
+        self.villkor_keyword_argument = villkor_keyword_argument
+    
+    def kolla_villkor(self, **extra_keyword_arguments):
+        alla_keyword_argument = {**extra_keyword_arguments, **self.villkor_keyword_argument}
+        return self.villkors_funktion(**alla_keyword_argument)
+
 class FlyttGraf():
-    def __init__(self, x, y, steg):
-        self.x=x
-        self.y=y
-        self.steg=steg
+    
+    def __init__(self, kx, ky, antal_steg):
+        self.kx=kx
+        self.ky=ky
+        self.antal_steg=antal_steg
+        
+    def hitta_graf_punkter(self,start_vektor2, graf_villkor_lista):
+        
+        graf_punkter=[]
+        graf_är_klar=False
+    
+        x=start_vektor2[0]
+        y=start_vektor2[1]
+        
+        for steg in range(self.antal_steg):
+            
+            x+=self.kx
+            y+=self.ky
+            steg+=1
+            
+            for graf_villkor in graf_villkor_lista:
+                if graf_villkor.kolla_villkor(vektor2=[x,y])==False:
+                    return graf_punkter
+                
+            graf_punkter.append((x,y))
+            
+        return graf_punkter
 
 
 class FlyttBeteende:
@@ -59,59 +99,22 @@ class FlyttBeteende:
         self.kan_döda=kan_döda
         self.flytt_graf= flytt_graf
         self.krav_funktion = krav_funktion
-    def ge_lista_på_möjliga_flyttar_med_krav(self):
+        
+    def ge_lista_på_möjliga_flyttar_med_krav(self,pjäs):
         if self.krav_funktion==None:
-            return self.ge_lista_på_möjliga_flyttar()
+            villkor_inanför_matris=Villkor(är_vektor2_i_matris, matris=pjäs.förälder)
+            villkor_kollision=Villkor(är_vektor2_en_icke_valid_kollision_med_annan_pjäs,schackbräde_matris=pjäs.förälder, beteende=self, pjäs=pjäs)
+            return self.flytt_graf.hitta_graf_punkter((pjäs.x,pjäs.y),[villkor_inanför_matris,villkor_kollision])
         return None
-    
-    def ge_lista_på_möjliga_flyttar(self):
-        
-        möjliga_flyttar=[]
-
-        pjäs=self.förälder
-        schack_position_matris=pjäs.förälder
-        flytt_graf=self.flytt_graf
-        
-        x=pjäs.x
-        y=pjäs.y
-        
-        steg=0
-        kolliderat_med_pjäs=False
-        utanför_matris=False
-        
-        while((utanför_matris==False) and (steg<flytt_graf.steg) and kolliderat_med_pjäs==False):
-            print(f'x{x}:y{y}')
-            x+=flytt_graf.x
-            y+=flytt_graf.y
-            steg+=1
-            
-            if är_vektor2_i_matris([x,y],schack_position_matris)==False:
-                utanför_matris=True
-                continue
-                                   
-            print(schack_position_matris[x][y].pjäs)
-            
-            if schack_position_matris[x][y].pjäs!=None:
-                kolliderat_med_pjäs=True
-                print(f'x{x}:y{y}')
-                print("crash")
-                if self.kan_döda==True:
-                    möjliga_flyttar.append((x,y))
-                continue
-            
-            möjliga_flyttar.append((x,y))
-        return möjliga_flyttar
-            
-    
-        
+       
 class Pjäs:
-    def __init__(self, namn, karaktär, förälder, x, y, team):
+    def __init__(self, namn, karaktär, förälder, x, y, färg):
         self.namn = namn
         self.karaktär = karaktär
         self.förälder=förälder
         self.x=x
         self.y=y
-        self.team=team
+        self.färg=färg
         self.beteende_lista=[]
         self.moves=0
 
@@ -126,7 +129,7 @@ class Pjäs:
     def ge_lista_på_alla_möjliga_flyttar(self):
         alla_möjliga_flyttar=[]
         for beteende in self.beteende_lista:
-            alla_möjliga_flyttar+=beteende.ge_lista_på_möjliga_flyttar_med_krav()
+            alla_möjliga_flyttar+=beteende.ge_lista_på_möjliga_flyttar_med_krav(self)
         return alla_möjliga_flyttar
     
 def skapa_pjäs_bonde(schack_bräde,x,y,vit):
@@ -174,8 +177,8 @@ class SchackPosition:
             return färg_katalog[self.färg]   
         return self.pjäs.hämta_utseende_sträng()
         
-def skapa_2d_matris(storlek): #Returnar en X*X matris, X=storlek
-    matris = [[0 for ix in range(storlek)] for iy in range(storlek)]
+def skapa_2d_matris(kolumner,rader): #Returnar en X*X matris, X=storlek
+    matris = [[0 for ix in range(rader)] for iy in range(kolumner)]
     return matris
 
 def fyll_2d_matris_med_tomma_schack_positioner(matris):
@@ -193,64 +196,75 @@ def gör_shack_position_matris_mönstrad(matris):
 def placera_standard_pjäser_i_shack_position_matris(matris):
     skapa_pjäs_bonde(matris,0,6,True)
     skapa_pjäs_bonde(matris,0,5,False)
-    skapa_pjäs_löpare(matris,3,3,False)
+    skapa_pjäs_löpare(matris,3,3,True)
     
 def skapa_standard_schackbräde_matris():
-    schackbräde_matris=skapa_2d_matris(8)
+    schackbräde_matris=skapa_2d_matris(8,8)
     fyll_2d_matris_med_tomma_schack_positioner(schackbräde_matris)
     gör_shack_position_matris_mönstrad(schackbräde_matris)
     placera_standard_pjäser_i_shack_position_matris(schackbräde_matris)
     return schackbräde_matris
-
-class SchackBräde():
-    matris=skapa_standard_schackbräde_matris()
     
-def rita_schack_bräde(schack_bräde):
+def rita_schack_bräde(schackbräde_matris):
     bräde_sträng=""
-    for iy in range(len(schack_bräde.matris[0])):
-        for ix in range(len(schack_bräde.matris)):
-            schack_position=schack_bräde.matris[ix][iy]
+    for iy in range(len(schackbräde_matris[0])):
+        for ix in range(len(schackbräde_matris)):
+            schack_position=schackbräde_matris[ix][iy]
             bräde_sträng+=schack_position.hämta_utseende_sträng()
         bräde_sträng+="\n"            
     print(bräde_sträng)
 
-def välj_vektor_position_i_shack_bräde(schack_bräde):
-    while True:
-        print("Välj en pjäs genom att skriva in bokstav(kolumn) och siffra(rad). T.ex. e2) \n")
+
+def välj_vektor_position_i_matris(matris):
+
         vald_sträng_position=input()
         
-        #try:
-        if vald_sträng_position=="exit":
-            exit()
-        if len(vald_sträng_position)>1:
-            if vald_sträng_position[0] in sträng_till_kolumn_katalog:
-                 if int(vald_sträng_position[1]) in range(1,len(schack_bräde.matris)+1):
-                     return sträng_position_till_vektor2(vald_sträng_position)
-                 else:
-                     print(f'Andra karaktären måste vara ett nummer {1}-{len(schack_bräde.matris)}')
-            else:
-                print("Första karaktären måste vara en liten bokstav a-h")
-       # except:
-           # print("Försök Igen!")
+        max_bredd=len(matris)
+        max_höjd=len(matris[0])
         
- 
+        try:
+            if vald_sträng_position=="exit":
+                return()
+            if len(vald_sträng_position)>1:
+                if vald_sträng_position[0] in sträng_till_kolumn_katalog:
+                     if int(vald_sträng_position[1]) in range(1,max_bredd):
+                         return sträng_position_till_vektor2(vald_sträng_position)
+                     else:
+                         print(f'Andra karaktären måste vara ett nummer {1}-{max_bredd}')
+                else:
+                    print(f'Första karaktären måste vara en liten bokstav {sträng_till_kolumn_katalog[0]}-{sträng_till_kolumn_katalog[max_höjd]}')
+        except:
+           print("Försök Igen!")
 
-def spela_schack_match(schack_bräde):
-    rita_schack_bräde(schack_bräde)
+def spela_schack_match(schackbräde_matris):
+    rita_schack_bräde(schackbräde_matris)
     match_pågår=True
     vald_position=None
     
     while match_pågår:
-        vald_vektor=välj_vektor_position_i_shack_bräde(schack_bräde)
-        vald_position=schack_bräde.matris[vald_vektor[0]][vald_vektor[1]]
-        if vald_position.pjäs==None:
-            vald_position=None     
-        else:
-            print(vald_position.pjäs.ge_lista_på_alla_möjliga_flyttar())
-        print(vald_position)
-    
+        
+        print("Välj en pjäs genom att skriva in bokstav(kolumn) och siffra(rad). T.ex. e2) \n")
+        vald_vektor=välj_vektor_position_i_matris(schackbräde_matris)
+        if vald_vektor!=None:
+            vald_position=schackbräde_matris[vald_vektor[0]][vald_vektor[1]]
+            if vald_position.pjäs==None:
+                vald_position=None     
+            else:
+                print(vald_position.pjäs.ge_lista_på_alla_möjliga_flyttar())
+            print(vald_position)
 
 
-spela_schack_match(SchackBräde())
+standard_schackbräde_matris=skapa_standard_schackbräde_matris()
+spela_schack_match(standard_schackbräde_matris)
 
+#Tänk kring att ha en lista på flyttkrav istället för att hardcoda saker i while. *check
+#Snygga till kod, ta bort onödiga moment *check
+
+#kommentera lite.
+#Kolla lag när man kan döda
+#Flytt funktion
+#Skriv klart alla pjäser
+#Positionera pjäser
+#Varanan tur system
+#Kan ej flytta motståndares pjäs under sin tur.
 
